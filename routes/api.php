bcd704512ffbe62eca74b987b1e8df796afdc8bc
Mandia <mandiasamuels@gmail.com>
2018-09-26 17:47:19 +0200

    Oracle text now serves only 1 card

--- routes/api.php
+++ routes/api.php
@@ -22,18 +22,39 @@ Route::middleware('auth:api')->get('/user', function (Request $request) {
 
 
 Route::get('/cards', function (){
+    $cards = "";
+
+    for ($x = 0; $x <= 1; $x++) {
+        $request = new GuzzleHttp\Psr7\Request (
+            "GET",
+            "https://api.scryfall.com/cards/random"
+
+        );
+
+        $guzzle = new GuzzleHttp\Client();
+        $result = $guzzle->send($request);
+
+        $randomCard = json_decode($result->getBody()->getContents());
+
+        $cards = $cards . " " . $randomCard->oracle_text;
+    }
+
+    return $cards;
+});
+
+
+Route::get('/testApi', function (){
 
     $request = new GuzzleHttp\Psr7\Request (
         "GET",
-        "https://api.scryfall.com/cards/random"
+            "https://cloudproject-217611.appspot.com/api/cards"
 
     );
 
     $guzzle = new GuzzleHttp\Client();
     $result = $guzzle->send($request);
 
-    $randomCard = json_decode($result->getBody()->getContents());
-    return $randomCard->name;
+    return $result->getBody()->getContents();
 });
 
 
